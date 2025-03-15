2b4450f5a634843802ffdbc5a27b8b4f8ed74e4a
festusrono <festusrono56@gmail.com>
2025-03-15 14:34:53 +0100

    js fetch and await response from api

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f00c38e0279b79c85480c3fe775d518c'
+$(document).ready(function () {
+    weatherFn('pune');
+});
+
+async function weatherFn(cName) {
+    const temp = '${url}?q=${cName}&appid={apiKey}&units=metric';
+    try {
+        const res = await fetch(temp);
+        const data = await res.json();
+        if (res.ok) {
+            weatherShowFn(data);
+
+        } else {
+            alert('City Not Found. Please Try Again');
+        }
+
+}
